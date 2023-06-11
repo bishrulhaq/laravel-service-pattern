1813d45e390849787e375588b7f13edf48d1f5c7
Bishrul Haq <bishhaq@gmail.com>
2023-06-11 12:26:20 +0530

    CRUD implemented with service class.

--- app/Http/Controllers/App/TaskDetailController.php
+++ app/Http/Controllers/App/TaskDetailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\App;
 
 use App\Enums\TaskDetailEndPoint;
+use App\Models\TaskDetail;
 use App\Requests\App\TaskDetailRequest;
 use App\Resources\App\TaskDetailResource;
 use App\Services\TaskDetail\TaskDetailService;
@@ -14,6 +15,18 @@ class TaskDetailController
     public function __construct(TaskDetailService $service)
     {
         $this->service = $service;
+    }
+
+    public function show(int $id): TaskDetailResource
+    {
+
+        $task = $this->service->show(
+            $id
+        );
+
+        return TaskDetailResource::make(
+            $task,
+        );
     }
 
     public function store(TaskDetailRequest $request): TaskDetailResource
@@ -29,5 +42,33 @@ class TaskDetailController
         return TaskDetailResource::make(
             $task,
         );
+    }
+
+    public function update(TaskDetailRequest $request, TaskDetail $taskDetail): TaskDetailResource
+    {
+
+        $task = $this->service->update(
+            $taskDetail,
+            $request->get('title'),
+            $request->get('description'),
+            $request->get('status')
+        );
+
+        return TaskDetailResource::make(
+            $task,
+        );
+    }
+
+    public function delete(int $id): TaskDetailResource
+    {
+
+        $task = $this->service->delete(
+            $id
+        );
+
+        return TaskDetailResource::make(
+            $task,
+        );
+
     }
 }
